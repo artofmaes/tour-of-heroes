4fcf3d4ca54a1059f3949ecf536ac8cf547209a4
Jordi Maes <jordi19@live.be>
2021-12-01 13:59:05 +0100

    Cleaning up unnecessary code

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Strange' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Captain Marvel' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Black Panther' },
+  { id: 16, name: 'Mr Fantastic' },
+  { id: 17, name: 'Magneto' },
+  { id: 18, name: 'Cyclops' },
+  { id: 19, name: 'Scarlet Witch' },
+  { id: 20, name: 'Vision' }
 ];
